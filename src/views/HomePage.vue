dd0a8cffed7035cf48a568ff89a4cd44e65f96f4
Clueless <brendvandeneynde@hotmail.be>
2024-07-02 15:47:21 +0200

    feat: Setup Login and Home views
    Configured Login.vue to render a LoginForm component for user authentication.
    
    Integrated HomePage.vue to display a HomeComp.vue component, setting up the basic structure for the login and home pages in the application.

---
+++ src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<script>
+import Home from '../components/HomeComp.vue';
+
+export default {
+    components: {
+        Home
+    }
+}
+</script>
+
+<template>
+    <div>
+        <Home />
+    </div>
+</template>
---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+  <script setup>
+  import LoginForm from '../components/LoginComp.vue';
+</script>
+
+<template>
+  <div>
+    <LoginForm />
+  </div>
+</template>
+
