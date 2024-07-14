421f94e72e102a8d34297fa98426fa09569a30a1
Clueless <brendvandeneynde@hotmail.be>
2024-07-14 17:22:09 +0200

    refactor: update navbar with button component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,21 +1,22 @@
 <script setup>
 import { useRouter } from 'vue-router';
-
-const router = useRouter();
+import Button from '../components/Button.vue';
 
 const handleLogout = () => {
     router.push({ name: 'Login' });
 };
+
+const router = useRouter();
 </script>
 
 <template>
     <nav>
         <ul>
             <li><router-link to="/">Home</router-link></li>
-            <li><router-link to="/basket">Basket</router-link></li>
             <li><router-link to="/configurator">Configurator</router-link></li>
+            <li><router-link to="/basket">Basket</router-link></li>
             <li><router-link to="/orderlist">Order List</router-link></li>
-            <li><button @click="handleLogout">Logout</button></li>
+            <li><Button type="log-out" @click="handleLogout">Logout</Button></li>
         </ul>
     </nav>
 </template>
@@ -34,17 +35,5 @@ ul {
 
 li {
     margin-right: 1rem;
-}
-
-button {
-    background-color: #ff6347;
-    color: white;
-    border: none;
-    padding: 0.5rem 1rem;
-    cursor: pointer;
-}
-
-button:hover {
-    background-color: #ff473a;
 }
 </style>
