9d872e1a5661f9b814012ad0005d2792c7eab824
Clueless <brendvandeneynde@hotmail.be>
2024-07-24 22:51:53 +0200

    create: register.vue page + add register component

---
+++ src/views/Register.vue
@@ -0,0 +1,19 @@
+<script setup>
+import RegisterForm from '../components/RegisterForm.vue';
+</script>
+
+<template>
+    <div class="register-page">
+        <RegisterForm />
+    </div>
+</template>
+
+<style scoped>
+.register-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f5f5f5;
+}
+</style>
