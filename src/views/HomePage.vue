de8c0da1d31576a570d59078867d1ec625bfd13f
Clueless <brendvandeneynde@hotmail.be>
2024-08-11 22:58:30 +0200

    fix: prevent horizontal & vertical scrolling

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,8 +4,19 @@ import Navbar from '../components/Navbar.vue';
 </script>
 
 <template>
-    <div>
+    <div class="page-container">
         <Navbar />
         <HomeComp />
     </div>
 </template>
+
+<style scoped>
+.page-container {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+</style>
